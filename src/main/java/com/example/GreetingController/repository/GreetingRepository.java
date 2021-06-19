979ffaa27424ebad55c82bbf6466d639663435c8
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-19 06:59:09 +0530

    Added : code to delete greeting message by id

--- src/main/java/com/example/GreetingController/repository/GreetingRepository.java
+++ src/main/java/com/example/GreetingController/repository/GreetingRepository.java
@@ -3,7 +3,6 @@ package com.example.GreetingController.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.GreetingController.entity.Greeting;
-import com.example.GreetingController.entity.User;
 
 public interface GreetingRepository extends JpaRepository<Greeting,Long> {
 
