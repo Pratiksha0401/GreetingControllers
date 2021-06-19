7b8ce580d3ea44d6640420b9cefdf45b0a12b47f
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-19 05:51:54 +0530

    Added: code to different messages using service layer

--- src/main/java/com/example/GreetingController/GreetingControllerApplication.java
+++ src/main/java/com/example/GreetingController/GreetingControllerApplication.java
@@ -1,42 +1,13 @@
 package com.example.GreetingController;
 
-<<<<<<< HEAD
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GreetingControllerApplication {
-
-=======
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.GreetingController.entityGreeting.Greeting;
-import com.example.GreetingController.repositary.GreetRepositary;
-
-@SpringBootApplication
-public class GreetingControllerApplication implements CommandLineRunner{
+public class GreetingControllerApplication{
 	
-	@Autowired
-	GreetRepositary greetRepositary;
-	
->>>>>>> 7a9fe0109f633c1511944f0c8e897de24aed5060
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingControllerApplication.class, args);
 	}
 
-<<<<<<< HEAD
-=======
-	@Override
-	public void run(String... args) throws Exception {
-		Greeting greet1 = new Greeting(1,"Hello World !"); 
-		
-		greetRepositary.greeting.addAll(Arrays.asList(greet1));
-	}
-	
->>>>>>> 7a9fe0109f633c1511944f0c8e897de24aed5060
 }
--- src/main/java/com/example/GreetingController/controller/GreetingControllers.java
+++ src/main/java/com/example/GreetingController/controller/GreetingControllers.java
@@ -4,6 +4,10 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +31,19 @@ public class GreetingControllers {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		User user = new User();
 		user.setFirstName(name);
-		return greetingService.addGreeting(user);
-		
+		return greetingService.addGreeting(user);		
+	}
+	
+	//UC3
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " !!";
+	}
+
+	@PutMapping("/put/{firstName}")
+	public String sayHello(@PathVariable String firstName,
+			               @RequestParam(value="lastName") String lastName) {
+		return "Hello" + firstName +" "+ lastName + " !!";
 	}
 
 }
